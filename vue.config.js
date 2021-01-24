ad25d05ccbad88c0d476d009f587963652d86cc3
Rodion <r.1on@yandex.ru>
2021-01-24 12:42:15 +0300

    first

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,6 @@
 module.exports = {
   publicPath: process.env.NODE_ENV === 'production'
-    ? '/dist'
+    ? './'
     : '/',
   assetsDir: 'assets',
   productionSourceMap: process.env.NODE_ENV === 'production' ? false : true,
